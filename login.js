c66cc7e97fb381933999cffb83b40be72bf473a4
davidudo <udodavid46.ud@gmail.com>
2022-04-22 13:51:06 +0100

    Renamed project_list.html to index.html

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ form.addEventListener('submit', (e) => {
             console.log('DATA:', data.access)
             if (data.access) {
                 localStorage.setItem('token', data.access)
-                window.location = 'https://devsearchapiweb.netlify.app/project_list.html'
+                window.location = 'https://devsearchapiweb.netlify.app/index.html'
             } else {
                 alert('Username OR password did not work')
             }
