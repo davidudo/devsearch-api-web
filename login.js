82b5ca2d1ae2395652b9127eef3419e6de9fa394
udodavid46.ud@gmail.com <udodavid46.ud@gmail.com>
2022-08-28 20:41:38 +0100

    Fixed successful login redirecting page link

--- login.js
+++ login.js
@@ -20,7 +20,7 @@ form.addEventListener('submit', (e) => {
             console.log('DATA:', data.access)
             if (data.access) {
                 localStorage.setItem('token', data.access)
-                window.location = 'https://davidudo.github.io/devsearch-api-web//index.html'
+                window.location = 'https://davidudo.github.io/devsearch-api-web/index.html'
             } else {
                 alert('Username OR password did not work')
             }
