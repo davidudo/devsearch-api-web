4eb845b453a789d8154e8815f99b7067f514a44f
udodavid46.ud@gmail.com <udodavid46.ud@gmail.com>
2022-08-28 22:27:00 +0100

    .

--- login.js
+++ login.js
@@ -1,31 +1,31 @@
 let form = document.getElementById('login-form')
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
+      e.preventDefault()
 
-    let formData = {
+      let formData = {
         'username': form.username.value,
         'password': form.password.value
-    }
+      }
 
-    fetch('https://devsearchweb.herokuapp.com/api/users/token/', {
-        method: 'POST',
-        headers: {
+      fetch('https://devsearchweb.herokuapp.com/api/users/token/', {
+          method: 'POST',
+          headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(formData)
-    })
+          },
+          body: JSON.stringify(formData)
+        })
         .then(response => response.json())
         .then(data => {
             console.log('DATA:', data.access)
             if (data.access) {
-                localStorage.setItem('token', data.access)
-                alert("You have been logged in successfully!")
-                window.history.back()
-                window.location.reload()
-            } else {
+              localStorage.setItem('token', data.access);
+                window.history.back();
+                window.location.reload();
+              }
+              else {
                 alert('Username OR password did not work')
-            }
-            return false
+              }
+              return false
+            })
         })
-})
